f6612b11577adc12d8edc02cf4f16584c01a7664
louie_gomez <kinggomez027.lg@gmail.com>
2022-03-17 16:22:50 -0400

    commit tree 270fefa6c34caf727c3a31d9cc15e12450a88b28

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > Hq) {
+        return pickup - Hq
+    } else return Hq - pickup
+}
+const Hq = 42;
+
+function distanceFromHqInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    if (pickup > Hq) {
+        return (pickup - Hq) * 264
+    } else return (Hq - pickup) * 264
+}
+const feet = 264;
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+    return (destination - start) * 264
+    } else return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+        return (0);
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    }else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        return (25);
+    }else {
+        return 'cannot travel that far';
+    }
+}
